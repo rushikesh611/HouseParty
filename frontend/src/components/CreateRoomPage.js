3f1fd7706f44bd4cc6d8f026c183559d1a91ef8e
hrushikesh jadhav <rushi611@gmail.com>
2021-06-15 00:13:53 +0530

    join room page

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
-import {
-  Typography,
-  Button,
-  Grid,
-  TextField,
-  FormHelperText,
-  FormControl,
-  Radio,
-  RadioGroup,
-  FormControlLabel,
-} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
 import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
   defaultVotes = 2;
+
   constructor(props) {
     super(props);
     this.state = {
       guestCanPause: true,
       votesToSkip: this.defaultVotes,
     };
+
     this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
     this.handleVotesChange = this.handleVotesChange.bind(this);
     this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
@@ -30,10 +30,11 @@ export default class CreateRoomPage extends Component {
       votesToSkip: e.target.value,
     });
   }
+
   handleGuestCanPauseChange(e) {
-    this.setState = {
+    this.setState({
       guestCanPause: e.target.value === "true" ? true : false,
-    };
+    });
   }
 
   handleRoomButtonPressed() {
@@ -47,8 +48,9 @@ export default class CreateRoomPage extends Component {
     };
     fetch("/api/create-room", requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.props.history.push("/room/" + data.code));
   }
+
   render() {
     return (
       <Grid container spacing={1}>
@@ -85,11 +87,14 @@ export default class CreateRoomPage extends Component {
         <Grid item xs={12} align="center">
           <FormControl>
             <TextField
-              onChange={this.handleVotesChange}
               required={true}
               type="number"
+              onChange={this.handleVotesChange}
               defaultValue={this.defaultVotes}
-              inputProps={{ min: 1, style: { textAlign: "center" } }}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
             />
             <FormHelperText component="div">
               <div align="center">Votes Required To Skip Song</div>
@@ -102,7 +107,7 @@ export default class CreateRoomPage extends Component {
             variant="contained"
             onClick={this.handleRoomButtonPressed}
           >
-            Create a Room
+            Create A Room
           </Button>
         </Grid>
         <Grid item xs={12} align="center">
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
+import Room from "./Room";
 import CreateRoomPage from "./CreateRoomPage";
 import {
   BrowserRouter as Router,
@@ -22,6 +23,7 @@ export default class HomePage extends Component {
           </Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room}/>
         </Switch>
       </Router>
     );
